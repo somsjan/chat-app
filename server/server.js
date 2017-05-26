11a8f86a8a4dd4f897f86ac9c6edeb5f777a71d6
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-26 14:42:46 +0200

    Added admin messages

--- server/server.js
+++ server/server.js
@@ -15,31 +15,44 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.on('createMessage', (newMessage) =>{
-        console.log(newMessage);
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat-app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined the channel',
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) =>{
+        console.log(message);
+
         io.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
-            createdAt: Math.round(new Date().getTime()/1000)
+            createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()/1000
+        // });
     })
 
-    // socket.on('createMessage', (newMessage) =>{
-    //     console.log('New message', newMessage);
+    // socket.emit('newMessage', {
+    //     from: 'server',
+    //     text: 'server text',
+    //     createdAt: Math.round(new Date().getTime()/1000)
     // });
-
-    socket.emit('newMessage', {
-        from: 'server',
-        text: 'server text',
-        createdAt: Math.round(new Date().getTime()/1000)
-    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
-
-
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
